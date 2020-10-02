eb5eb47a4e20ea235c3f0f6301fb42d4a14e51bb
Annalise <59459636+Annalise-M@users.noreply.github.com>
2020-10-02 11:27:15 -0700

    Auth routes (#2)
    * user model placed
    
    * users tables added
    
    * testing added

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
 const pool = require('../lib/utils/pool');
-const request = require('supertest');
-const app = require('../lib/app');
+// const request = require('supertest');
+// const app = require('../lib/app');
 
 describe('alchemy-app routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+  it('is a dummy test', () => {
+    expect(true).toEqual(true);
   });
 });
---
+++ __tests__/user.test.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+const pool = require('../lib/utils/pool');
+const request = require('supertest');
+const app = require('../lib/app');
+// const UserService = require('../lib/services/user-service');
+
+describe('Auth routes', () => {
+  beforeEach(() => {
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('signup a user via POST', async() => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'test@test.com',
+        password: 'password',
+        phoneNumber: '7078675309',
+      });
+
+    expect(response.body).toEqual({
+      userId: expect.any(String),
+      email: 'test@test.com',
+      phoneNumber: '7078675309',
+    });
+  });
+  
+  // it('logs in a user via POST', async() => {
+  //   const user = await UserService.create({
+  //     email: 'test@test.com',
+  //     password: 'password',
+  //     phoneNumber: '7078675309',
+  //   });
+
+  //   const response = await request(app)
+  //     .post('/api/v1/auth/login')
+  //     .send({
+  //       email: 'test@test.com',
+  //       password: 'password',
+  //     });
+
+  //   expect(response.body).toEqual({
+  //     userId: user.userId,
+  //     email: 'test@test.com',
+  //     phoneNumber: '7078675309',
+  //   });
+  // });
+
+  // it('verifies a user via GET', async() => {
+  //   const agent = request.agent(app);
+  //   await agent
+  //     .post('/api/v1/auth/signup')
+  //     .send({
+  //       email: 'test@test.com',
+  //       password: 'password',
+  //       phoneNumber: '7078675309',
+  //     });
+
+  //   const response = await agent
+  //     .get('/api/v1/auth/verify');
+
+  //   expect(response.body).toEqual({
+  //     userId: expect.any(String),
+  //     email: 'test@test.com',
+  //     phoneNumber: '7078675309',
+  //   });
+
+  //   const responseWithoutAUser = await request(app)
+  //     .get('/api/v1/auth/verify');
+
+  //   expect(responseWithoutAUser.body).toEqual({
+  //     status: 500,
+  //     message: 'jwt must be provided',
+  //   });
+  // });
+});
+
+
