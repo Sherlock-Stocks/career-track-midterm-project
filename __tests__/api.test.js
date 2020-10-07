c720c5f793f66f4dffc86ada774f53ea34fb689b
Patrick Wilson <pmwilson333@gmail.com>
2020-10-06 17:34:49 -0700

    completed api test

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -6,9 +6,10 @@ describe('api-calls routes', () => {
     const apiCall = await request(app)
       .post('/api/v1/data/TSLA')
       .send({
-        startDate: '2020/01/02',
-        endDate: '2020/02/01',
+        startDate: '2020/02/01',
+        endDate: '2020/02/03',
         frequency: '1day'
       });
+    expect(apiCall.text).toEqual('{"meta":{"symbol":"TSLA","interval":"1day","currency":"USD","exchange_timezone":"America/New_York","exchange":"NASDAQ","type":"Common Stock"},"values":[{"datetime":"2020-02-03","open":"134.73800","high":"157.22800","low":"134.70400","close":"156.00000","volume":"47233500"}],"status":"ok"}');
   });
 });
