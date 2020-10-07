1cfbf6c0a00ba9476cc53f64302d37341a90d139
Patrick Wilson <pmwilson333@gmail.com>
2020-10-06 18:54:59 -0700

    removed errors to merge

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,5 +1,4 @@
-const fs = require('fs');
-const pool = require('../lib/utils/pool');
+
 const request = require('supertest');
 const app = require('../lib/app');
 const UserService = require('../lib/services/user-service');
